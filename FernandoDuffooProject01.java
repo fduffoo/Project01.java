392224eb3d28ba7e38fcb118fdd6f3c604572c78
Fernando Duffoo <89112257+fduffoo@users.noreply.github.com>
2022-08-21 18:47:44 -0400

    Add files via upload

---
+++ FernandoDuffooProject01.java
@@ -0,0 +1,18 @@
+
+/**
+ * description : This programs displays "Hello, World!" to the console.
+ * author      : Fernando Duffoo
+ * date        : 6/9/22
+ */
+public class FernandoDuffooProject01
+{
+    // instance variables - replace the example below with your own
+    public static void main(String [] args){
+        System.out.println("Hello, World!");
+        System.out.println("This is my first Java Program");
+    }
+}
+
+//#6 Nothing happens when changing the statement to "System to system", just changes the contents within the quotations to be displayed.
+//#7 Error, this creates an unclosed String. Causes both lines to have errors.
+//#8 This change in the statement caused no syntax errors.
